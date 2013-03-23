b809b46abee9effeb3e6702a514b7e5e00a50d2d
relan <relan@users.noreply.github.com>
2013-03-23 14:48:34 +0000

    Move platform-specific code into separate units

---
+++ dump/platform.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2013  Andrew Nayenko
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stddef.h>
+
+void get_date(char *buffer, size_t size);
+void get_computer_model(char *buffer, size_t size);
+void get_ram(char *buffer, size_t size);
+void get_os(char *buffer, size_t size);
+void get_kernel(char *buffer, size_t size);
