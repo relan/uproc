24e8d1ba8f160f89f9288aa91e2d573b06cdc6fc
relan <relan@users.noreply.github.com>
2013-10-09 06:52:04 +0000

    Add more MSRs

--- dump/x86msr.c
+++ dump/x86msr.c
@@ -47,10 +47,23 @@ void dump_x86_msr(void)
 	puts("");
 	puts("MSR dump:");
 
+	dump_msr(fd, "IA32_FEATURE_CONTROL", 0x3a);
+	dump_msr(fd, "IA32_MISC_ENABLE", 0x1a0);
 	dump_msr(fd, "IA32_EFER", 0xc0000080);
+	dump_msr(fd, "IA32_VMX_BASIC", 0x480);
 	dump_msr(fd, "IA32_VMX_PINBASED_CTLS", 0x481);
 	dump_msr(fd, "IA32_VMX_PROCBASED_CTLS", 0x482);
 	dump_msr(fd, "IA32_VMX_PROCBASED_CTLS2", 0x48b);
+	dump_msr(fd, "IA32_VMX_EXIT_CTLS", 0x483);
+	dump_msr(fd, "IA32_VMX_ENTRY_CTLS", 0x484);
+	dump_msr(fd, "IA32_VMX_MISC", 0x485);
+	dump_msr(fd, "IA32_VMX_VMCS_ENUM", 0x48a);
+	dump_msr(fd, "IA32_VMX_EPT_VPID_CAP", 0x48c);
+	dump_msr(fd, "IA32_VMX_TRUE_PINBASED_CTLS", 0x48d);
+	dump_msr(fd, "IA32_VMX_TRUE_PROCBASED_CTLS", 0x48e);
+	dump_msr(fd, "IA32_VMX_TRUE_EXIT_CTLS", 0x48f);
+	dump_msr(fd, "IA32_VMX_TRUE_ENTRY_CTLS", 0x490);
+	dump_msr(fd, "IA32_VM_CR", 0xc0010114);
 
 	if (fd != -1)
 		close(fd);
